5c639d24a5ac6dd687402d6cf20751138ca8885f
ggerla <giuseppe.gerla@gmail.com>
2014-11-28 14:36:30 +0100

    Added license reference to test source code files

--- src/test/java/com/google/devtools/java2csharp/BuilderTest.java
+++ src/test/java/com/google/devtools/java2csharp/BuilderTest.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2014 Giuseppe Gerla. All Rights Reserved.
+ *
+ * Licensed under the GNU General Public License, Version 2.0;
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.gnu.org/licenses/gpl2.txt
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.google.devtools.java2csharp;
 
 import static org.junit.Assert.*;
